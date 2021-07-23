d44dee2c319c8a9bac53dc59e72ad6493c128cec
seraph92 <11453441+seraph92@users.noreply.github.com>
2021-07-24 04:22:30 +0900

    Menu 테스트중

--- MenuScene.py
+++ MenuScene.py
@@ -4,6 +4,9 @@ from Background import *
 from BKLOG import *
 from Scenes import *
 
+import pygame_menu as pgm
+from pygame_menu.examples import create_example_window
+
 class MenuScene(Scene):
     def __init__(self, name, gamepad):
         DEBUG("<< Enter")
@@ -27,6 +30,25 @@ class MenuScene(Scene):
         DEBUG(" Exit>>")
         return nextScene
 
+    def set_difficulty(selected, value):
+        """
+        Set the difficulty of the game.
+        :return: None
+        """
+        print('Set difficulty to {} ({})'.format(selected[0], value))
+
+    def start_the_game(self):
+        """
+        Function that starts a game. This is raised by the menu button,
+        here menu can be disabled, etc.
+        :return: None
+        """
+        self.user_name
+        print('{0}, Do the job here!'.format(self.user_name.get_value()))
+
+    def draw_background(self):
+        self.bg.update(self.gamepad)
+
     def event_loop(self):
         '''
         IF event.type == pygame.QUIT
@@ -44,7 +66,23 @@ class MenuScene(Scene):
         pygame.quit()
         quit() # 종료
         '''
+
+        # surface = create_example_window('Example - Simple', (600, 400))
+
+        menu = pgm.Menu(
+          height=300,
+          theme=pgm.themes.THEME_BLUE,
+          title='Welcome',
+          width=400
+        )
+
+        user_name = menu.add.text_input('Name: ', default='John Doe', maxchar=10)
+        menu.add.selector('Difficulty: ', [('Hard', 1), ('Easy', 2)], onchange=self.set_difficulty)
+        menu.add.button('Play', self.start_the_game)
+        menu.add.button('Quit', pgm.events.EXIT)
+
         DEBUG("<< Enter")
+        '''
         OnGoing = True
         while OnGoing:
             for event in pg.event.get():
@@ -55,7 +93,6 @@ class MenuScene(Scene):
                     #키보드 제어권 전달
                     if event.key == pg.K_SPACE:
                         OnGoing = False
-
             # 배경 초기화
             self.gamepad.fill(WHITE)
             # 배경 그리기
@@ -65,6 +102,11 @@ class MenuScene(Scene):
             pg.display.update()
             # refresh rate 보정
             self.clock.tick(REFRESH_RATE)
+
+        '''
+        menu.mainloop(self.gamepad, self.draw_background)
+        #menu.mainloop(self.gamepad, self.draw_background, disable_loop=True, fps_limit=60)
+        #main_menu.mainloop(surface, main_background, disable_loop=test, fps_limit=FPS)
 
         nextScene = "Stage1"
         DEBUG(" Exit>>")
